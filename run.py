2a8d9b23a18cd818cb09039c04def7cd414e985b
kimhappy <kimsvrkimsvr@gmail.com>
2024-05-28 16:14:05 +0900

    add: amp name

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from sf_train      import Data, ModelConfig, TrainConfig, Train
 from sf_train.util import read_mono, write_mono
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print(f'Usage: python3 {sys.argv[ 0 ]} <train_input> <train_target> <val_input> <val_target>')
+    if len(sys.argv) != 6:
+        print(f'Usage: python3 {sys.argv[ 0 ]} <train_input> <train_target> <val_input> <val_target> <name>')
         sys.exit(1)
 
     model_config = ModelConfig(
@@ -37,6 +37,7 @@ if __name__ == '__main__':
 
     patience_counter =    0
     best_vali_loss   = 1e12
+    file_name        = f'{model_config.sample_rate}_{data.num_params}_{model_config.hidden_size}_{sys.argv[ 5 ]}'
 
     for epoch in range(1, MAX_EPOCHS + 1):
         tr = train.train()
@@ -51,8 +52,8 @@ if __name__ == '__main__':
         if vr.loss < best_vali_loss:
             patience_counter = 0
             best_vali_loss   = vr.loss
-            train.save(f'result/{epoch}.snow')
-            write_mono(f'result/{epoch}.wav', vr.processed, model_config.sample_rate)
+            train.save(f'{file_name}.snow')
+            write_mono(f'{file_name}.wav', vr.processed, model_config.sample_rate)
             continue
 
         patience_counter += 1
--- sf_train/_model_config.py
+++ sf_train/_model_config.py
@@ -2,8 +2,6 @@ class _ModelConfig:
     def __init__(
         self               ,
         sample_rate = 48000,
-        output_size =     1,
         hidden_size =    64):
         self.sample_rate = sample_rate
-        self.output_size = output_size
         self.hidden_size = hidden_size
